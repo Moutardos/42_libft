2a5125298f9e893a95e13191055c8298aa7715fc
Loic COZDENMAT <lcozdenm@e2r11p10.clusters.42paris.fr>
2022-11-20 13:06:28 +0100

    ft_split over ! Bonus part missing

---
+++ ft_lstnew.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcozdenm <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 12:53:59 by lcozdenm          #+#    #+#             */
+/*   Updated: 2022/11/20 13:02:54 by lcozdenm         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	new;
+
+	new = malloc(sizeof(t_list));
+	if (new == NULL)
+		return (NULL);
+	new->content = content;
+	return (new);
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,11 @@
 /*   By: lcozdenm <loic.cozdenmat@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 22:01:30 by lcozdenm          #+#    #+#             */
-/*   Updated: 2022/11/19 23:59:34 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2022/11/20 10:34:17 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 /* return 1 if c is in str */
 static	int	ft_char_in_str(char c, char const *str)
@@ -25,11 +24,10 @@ static	int	ft_char_in_str(char c, char const *str)
 	return (0);
 }
 
-/* return the size of the new string in strtrim and set start and end to the beginning and the end of 
- * the trimmed part */
-static size_t	ft_strtrim_wc(char const *s1, char const *set, size_t *start, size_t *end)
+/* set start and end to the index of the trimmed part, return the size of it */
+static size_t	wc(char const *s1, char const *set, size_t *start, size_t *end)
 {
-	size_t 		i;
+	size_t	i;
 
 	i = 0;
 	while (*s1 && ft_char_in_str(*s1, set))
@@ -58,15 +56,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 	size_t	size;
 	size_t	start;
 	size_t	end;
-	size_t	i;
-	
-	i = 0;
+
 	if (s1 == NULL || set == NULL)
 		return (NULL);
-	size = ft_strtrim_wc(s1, set, &start, &end);
+	size = wc(s1, set, &start, &end);
 	if (size + 1 > SIZE_MAX)
 		return (NULL);
-	res = malloc(size + 1);	
+	res = malloc(size + 1);
 	if (res == NULL)
 		return (NULL);
 	ft_strlcpy(res, (s1 + start), size + 1);
