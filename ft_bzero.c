85903f2b77b147870ebd7c9e30bcbca34fd98a8c
Loic COZDENMAT <lcozdenm@e2r11p10.clusters.42paris.fr>
2022-11-20 19:49:08 +0100

    just

--- ft_bzero.c
+++ ft_bzero.c
@@ -6,19 +6,21 @@
 /*   By: lcozdenm <loic.cozdenmat@gmail.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 20:43:44 by lcozdenm          #+#    #+#             */
-/*   Updated: 2022/11/20 18:16:01 by lcozdenm         ###   ########.fr       */
+/*   Updated: 2022/11/20 19:41:09 by lcozdenm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*mem_s;
 
+	mem_s = (unsigned char *) s;
 	i = 0;
 	while (i < n)
 	{
-		((unsigned char *) s)[i] = 0;
+		mem_s[i] = 0;
 		i++;
 	}
 }
